aaf109906edbd6b7a1d29c7470ede64cd045ced4
Krishna Kompalli <kompalligk@gmail.com>
2020-06-08 02:04:51 +0530

    modified model

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -2,6 +2,11 @@ export class Product {
   id: number;
   product_name: string;
   product_price: number;
+  department: any;
+  product_material;
+  product;
+  product_color;
+  product_adjective;
 
   constructor(id, product_name, product_price = 0) {
     this.id = id
